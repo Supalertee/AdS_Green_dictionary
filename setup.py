65147be4084b36629e2a4f6fb015e684b217a1fb
Supalertee <69739931+Supalertee@users.noreply.github.com>
2024-07-17 02:16:56 +0900

    add installer

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from Greenf_dict import __version__
+
+setup(
+    name='Greenf_dict',
+    version=__version__,
+
+    url='https://github.com/Supalertee/Green_dict',
+    author='Sukrakarn Suaplert',
+    author_email='supalertee.com',
+
+    py_modules=['Greenf_dict'],
+)
